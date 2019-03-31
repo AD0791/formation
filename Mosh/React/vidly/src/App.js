098e7c38c9a5b6b479c40bb395c591e791f0c8d3
Alexandro Disla <alexandrodisla@hotmail.com>
2019-03-31 08:49:17 -0400

    vidly projects

---
+++ Mosh/React/vidly/src/App.js
@@ -0,0 +1,18 @@
+import React, {
+  Component
+} from "react";
+//import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return ( <
+      main ClassName = "container" >
+      <
+      h1 > Hello World < /h1> < /
+      main >
+    );
+  }
+}
+
+export default App;
