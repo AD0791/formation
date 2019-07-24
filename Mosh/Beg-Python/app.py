f2d358f3963ec75cd210bd749cd238d602c51bc5
Alexandro Disla <alexandrodisla@hotmail.com>
2019-07-24 16:56:30 -0400

    lambda data structures

--- Mosh/Beg-Python/app.py
+++ Mosh/Beg-Python/app.py
@@ -1,11 +1,8 @@
-#import pdb
+# import pdb
 # pdb.run("test")
-age = [1, 2, 3, 4, 5, 6, 8, 9,10,11]
-needAge = 10
-for ages in age:
-    print(f'The current age is: {ages}')
-    if ages == needAge:
-        print(f"We find the desired age: {ages}")
-        break
-else:
-    print(f"The needed age {needAge} is not there.")
+
+numbers = [12, 32, 43, 25, 56, 76, 90]
+numbers.sort()
+print(numbers)
+numbers.sort(reverse=True)
+print(numbers)
