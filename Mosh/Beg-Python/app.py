dbd8152c3a7451f9af234a76d4a232903cd4ea16
Alexandro Disla <alexandrodisla@hotmail.com>
2019-07-28 09:26:30 -0400

    Inheritance

--- Mosh/Beg-Python/app.py
+++ Mosh/Beg-Python/app.py
@@ -1,75 +1,20 @@
-class TagCloud:
-    def __init__(self):
-        self.tags = {}
+class Product:
 
-    # ajouter tags
-    def add_tags(self, tag):
-        self.tags[tag.lower()] = self.tags.get(tag.lower(), 0) + 1
+    def __init__(self, price):
+        # we can use price like a regular property now
+        self.price = price
 
-    # read a tag like this instance["tag"].
-    # __getitem__ seems to be on the documentation.
-    # In [9]: cloud["python"]
-    # Out[9]: 6
-    # We can only read only
-    def __getitem__(self, tag):
-        return self.tags.get(tag.lower(), 0)
+    # getter
+    @property
+    def price(self):
+        return self.__price
 
-    # we need to implement that behavior instance["tag"] = value/count
-    # __setitem__ method need to be implemented
-    def __setitem__(self, tag, count):
-        self.tags[tag.lower()] = count
-
-    # get the lenght of the instance
-    def __len__(self):
-        return len(self.tags)
-
-    # allow the instance to be iterable.
-    def __iter__(self):
-        return iter(self.tags)
+    # setter
+    @price.setter
+    def price(self, value):
+        if value < 0:
+            raise ValueError("can't be negative number")
+        self.__price = value
 
 
-# Testing the class
-cloud = TagCloud()
-
-cloud.add_tags("Python")
-cloud.add_tags("Python")
-cloud.add_tags("Python")
-cloud.add_tags("Python")
-cloud.add_tags("Python")
-cloud.add_tags("Python")
-cloud.add_tags("Python")
-cloud.add_tags("Javascript")
-cloud.add_tags("Javascript")
-cloud.add_tags("Javascript")
-cloud.add_tags("Javascript")
-cloud.add_tags("Javascript")
-
-cloud.tags
-
-cloud["python"]
-
-cloud["Javascript"]
-
-cloud["Ruby"] = 6
-
-cloud.tags
-
-cloud["Javascript"] = 7
-
-cloud.tags
-
-cloud["Ruby"] = 7
-
-cloud.tags
-
-cloud.__getitem__("ruby")
-
-cloud.__setitem__("R", 3)
-
-cloud.tags
-
-len(cloud)
-
-# Iteration
-for val in cloud:
-    print(val, cloud.tags[val])
+prix = Product(10)
