db667a59561f4431fb26ae9afa47d9cfaedf246b
Alexandro Disla <alexandrodisla@hotmail.com>
2019-08-05 17:05:42 -0400

    models/migrations - change models

--- Mosh/Beg-Python/vidly/movies/models.py
+++ Mosh/Beg-Python/vidly/movies/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Genre(models.Model):
+    # use field
+    name = models.CharField(max_length=255)
+
+
+class Movie(models.Model):
+    # use field
+    title = models.CharField(max_length=255)
+    release_year = models.IntegerField()
+    number_in_stock = models.IntegerField()
+    daily_rate = models.FloatField()
+    # association has-many and belongs to
+    # on delete will tell django how to handle
+    # a delete event of a genre given the
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
