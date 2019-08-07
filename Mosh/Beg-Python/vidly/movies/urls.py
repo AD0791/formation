c80a468155d3647d89c2d8a47e88a6d5764901d8
Alexandro Disla <alexandrodisla@hotmail.com>
2019-08-07 19:52:22 -0400

    creating API

--- Mosh/Beg-Python/vidly/movies/urls.py
+++ Mosh/Beg-Python/vidly/movies/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from . import views
+
+app_name = 'movies'
+
 urlpatterns = [
-    path('', views.index, name='movies_index'),
+    path('', views.index, name='index'),
     # we have set django to apply this behavior:
     # movies/movie_id
     # make sure it's an integer
-    path('<int:movie_id>', views.detail, name='movies_detail')
+    # no need for movies_index and movies_detail
+    # the app_name property insure that django will
+    # know our to deals with these urls.
+    path('<int:movie_id>', views.detail, name='detail')
 ]
